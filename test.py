b78f1dd5d495b366d64fca9fecbfbc9b96876436
gitester2023 <isassanfa@gmail.com>
2023-05-19 19:27:59 +0330

    Update test.py
    add saman farahani print code

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 print("Add your name with a simple print like code below")
 print("Sassan Farahani!!!")
 
-#This is Test By seyedAli mousavi jazy 
+# This is Test By seyedAli mousavi jazy 
 a = 'SeyedAli'
 b=  'mousavi'
 print(a,b)
+# Test By Saman Farahani
+print("Saman Farahani")
